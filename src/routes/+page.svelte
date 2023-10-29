56d319ea9d52dc41960040f0e4593aa8774c491d
michivo <michivo@gmail.com>
2023-10-29 15:17:36 +0100

    start with login form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { signInWithEmailAndPassword } from "firebase/auth";
+	import { firebaseAuth } from "../firebase/firebase";
+    import { Button, Form, FormGroup, Input, Label } from 'sveltestrap';
+
+    let email = '';
+    let password = '';
+
+    $: detailsMissing = !email || !password;
+
+    async function signIn() {
+        const auth = firebaseAuth;
+        const result = await signInWithEmailAndPassword(auth, email, password);
+        console.error(result);
+        // const provider = new EmailAuthProvider();
+        // await signInWith(auth, provider).then((result) => {
+        //     console.error(result);
+        // });
+    }
+</script>
+
+<div>
+    <Form>
+        <FormGroup>
+            <Label for="username">Email</Label>
+            <Input id="username" type="text" bind:value={email} />
+        </FormGroup>
+        <FormGroup>
+            <Label for="password">Passwort</Label>
+            <Input id="password" type="password" bind:value={password} />
+          </FormGroup>
+        <Button bind:disabled="{ detailsMissing }" on:click={ signIn }>Anmelden</Button>
+    </Form>
+</div>
