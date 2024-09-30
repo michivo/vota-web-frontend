c42f177f12d8fe51749cfa1748ca0410f3ae3a15
Michael Faschinger <mfaschinger@protossecurity.com>
2024-09-30 11:27:31 +0200

    allow API access in csp

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from "@sveltejs/kit";
 
 const securityHeaders = {
     'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-    'Content-Security-Policy': 'default-src \'self\'; style-src \'self\' \'unsafe-inline\'; script-src \'self\' \'unsafe-inline\'',
+    'Content-Security-Policy': 'default-src \'self\'; style-src \'self\' \'unsafe-inline\'; script-src \'self\' \'unsafe-inline\'; connect-src \'self\' https://vota-api.faschinger.com/ https://vota-api.gruene.at/',
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
